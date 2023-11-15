491df1438ca3a764f751ab0ffe8d558232d9ac0d
Russell Gordon <rgordon@lcs.on.ca>
2023-11-15 09:57:02 -0500

    Added a stored property to hold a value. About to show it on screen.

--- GeometryHelper/ContentView.swift
+++ GeometryHelper/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var number: Double = 10.0
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("75.0")
+                .font(.largeTitle)
+                .bold()
         }
         .padding()
     }
