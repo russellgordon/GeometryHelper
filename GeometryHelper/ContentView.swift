73c21e79f62ea7475495d13953ab28db4c6fa924
Russell Gordon <rgordon@lcs.on.ca>
2023-11-15 09:50:10 -0500

    Initial Commit

---
+++ GeometryHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometryHelper
+//
+//  Created by Russell Gordon on 2023-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Russell Gordon on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
